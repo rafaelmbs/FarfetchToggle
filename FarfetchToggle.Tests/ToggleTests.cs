2073273ce12bc5a13de3bc275eaf43eb03b4a28c
Rafael Mendes <rafaelmbs@me.com>
2017-11-15 22:44:40 -0200

    Create get toggle response test

---
+++ FarfetchToggle.Tests/ToggleTests.cs
@@ -0,0 +1,29 @@
+﻿using FarfetchToggle.Contracts.Toggle;
+using FarfetchToggle.Services;
+using Xunit;
+
+namespace FarfetchToggle.Tests
+{
+    public class ToggleTests
+    {
+        private readonly ToggleService _service;
+
+        public ToggleTests(ToggleService service)
+        {
+            _service = service;
+        }
+
+        [Fact]
+        public void IsAValidToggleGetResponse()
+        {
+            //Arrange
+            ToggleGetResponse response = new ToggleGetResponse();
+
+            //Act
+            var result = _service.GetToggles();
+
+            //Assert
+            Assert.Same(result, response);
+        }
+    }
+}
